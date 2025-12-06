35bd6cd962811325c9d19d14f50ce44c84999b73
Petar Pavlović <p.petar.pavlovic.96@gmail.com>
2025-12-06 22:18:47 +0100

    Removed target/*

--- chapter_9/errors/target/debug/deps/errors-ba2b72d008b02abd.d
+++
@@ -1,5 +0,0 @@
-/home/ppetar/projects/learning_rust/chapter_9/errors/target/debug/deps/errors-ba2b72d008b02abd.d: src/main.rs
-
-/home/ppetar/projects/learning_rust/chapter_9/errors/target/debug/deps/errors-ba2b72d008b02abd: src/main.rs
-
-src/main.rs:
--- chapter_9/errors/target/debug/errors.d
+++
@@ -1,1 +0,0 @@
-/home/ppetar/projects/learning_rust/chapter_9/errors/target/debug/errors: /home/ppetar/projects/learning_rust/chapter_9/errors/src/main.rs
